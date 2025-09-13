ac0e262bc2bf95969a9f61a5937c0267d548cb74
Ha-im <gungsun1@naver.com>
2025-09-13 17:47:41 +0900

    🔧 Tstory 아이콘 없어도 뱃지 생성되도록 수정중

--- badge-maker.js
+++ badge-maker.js
@@ -24,7 +24,7 @@ const techStack = [
   { name: 'CSS3', color: 'blue', icon: si.css3 },
   { name: 'jQuery', color: 'lightblue', icon: si.jquery },
   { name: 'Bootstrap', color: 'purple', icon: si.bootstrap },
-  { name: 'Swiper.js', color: 'blue', logoBase64: Buffer.from(swiperSvg).toString('base64') },
+  { name: 'Swiper.js', color: 'blue', logoBase64: `data:image/svg+xml;base64,${Buffer.from(swiperSvg).toString('base64')}` },
   { name: 'Full.js', color: 'orange', icon: si.fullcalendar },
 
   // Backend / DB
@@ -40,7 +40,7 @@ const techStack = [
   { name: 'Git', color: 'red', icon: si.git },
   { name: 'GitHub', color: 'black', icon: si.github },
   { name: 'Figma', color: 'red', icon: si.figma },
-  { name: 'Tistory', color: 'black', logoBase64: Buffer.from(tistorySvg).toString('base64') },
+  { name: 'Tistory', color: 'black'},
   { name: 'Vercel', color: 'black', icon: si.vercel }
 ];
 
@@ -51,7 +51,7 @@ techStack.forEach(tech => {
     message: '100%',
     color: tech.color,
     style: 'flat-square',
-    logoBase64: tech.logoBase64 || (tech.icon ? Buffer.from(tech.icon.svg).toString('base64') : undefined)
+    logoBase64: tech.logoBase64 || (tech.icon ? `data:image/svg+xml;base64,${Buffer.from(tech.icon.svg).toString('base64')}` : undefined)
   };
 
   try {
